ee5198cb821b4dad7e062b3933d0c91580df0469
stanojlovic.marko@gmail.com <stanojlovic.marko@gmail.com>
2019-05-07 18:15:26 +0200

    py:grade school v2

--- python/grade-school/grade_school_v1.py
+++ python/grade-school/grade_school_v1.py
@@ -1,15 +1,17 @@
 from collections import defaultdict
-from itertools import chain
 
 class School(object):
     def __init__(self):
         self.school = defaultdict(list)
 
     def add_student(self, name, grade):
-        self.school[grade].append(name)
+        self.school[grade] += [name]
+
+    def roster(self):
+        results = []
+        for grade in sorted(self.school.keys()):
+            results += sorted(self.school[grade])
+        return results
 
     def grade(self, grade_number):
         return sorted(self.school[grade_number])
-
-    def roster(self):
-        return list(chain.from_iterable([self.grade(grade_num) for grade_num in sorted(self.school.keys())]))
