7c6aa64b2823c0b01bacc1792034ac2be0c05215
Marko Stanojlovic <marko.stanojlovic@workday.com>
2020-03-19 20:15:16 +0100

    clock

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,19 +1,20 @@
-from datetime import datetime, timedelta
-
-class Clock(object):
+class Clock:
     def __init__(self, hour, minute):
-        self.time = datetime(2019,1,1, (hour + minute//60)%24, minute%60)
+        self.hours, self.minutes = divmod((hour*60 + minute)%(24*60), 60)
 
-    def __repr__(self):
-        return self.time.strftime("%H:%M")
+    @property
+    def now(self):
+        return f"{self.hours:02}:{self.minutes:02}"
+
+    def __str__(self):
+        return self.now
 
     def __eq__(self, other):
-        return self.time == other.time
+        return self.now == other
 
     def __add__(self, minutes):
-        self.time += timedelta(minutes=minutes)
-        return repr(self)
+        return Clock(self.hours, self.minutes + minutes)
 
     def __sub__(self, minutes):
-        self.time -= timedelta(minutes=minutes)
-        return repr(self)
+        return Clock(self.hours, self.minutes - minutes)
+
